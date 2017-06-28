e2fec3a9315f00c871a056380ca3bfe7df9784ca
Peter Johnson <pete.johnsn0@gmail.com>
2017-06-27 19:42:13 -0600

    added yes/no buttons with methods that remove/add recipes

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Card, Container, Segment, Loader, Dimmer } from 'semantic-ui-react';
+import { Card, Container, Segment, Loader, Dimmer, Grid, Button, Icon } from 'semantic-ui-react';
 import { getRecipes } from '../actions/recipe'
 import axios from 'axios';
 
 
 class Game extends React.Component {
-    state = { loading: true, recipes: [], matchedRecipes: [] }
+    state = { loading: true, sidebyside: false, recipes: [], matchedRecipes: [] }
 
-    componentDidMount(id) {
-       axios.get(`/api/recipes/${id}`)
+    componentDidMount() {
+       axios.get(`/api/recipes/${this.props.id}`)
             .then((res) => {
                 const recipesData = res.data
                 this.setState({ recipes: recipesData, loading: false })
@@ -17,10 +17,27 @@ class Game extends React.Component {
         )
     }
 
-    // getCard = () => {
-    //     let recipe = this.state.recipes[0];
-    //     return (<div>{recipe.recipeName}</div>)
-    // }
+    checkRecipiesArray = () => {
+        let recipes = this.state.recipes
+        if (recipes.length === 0) {
+            return this.setState({ sidebyside: true }) 
+        } return
+    }
+
+    removeRecipe = () => {
+        let updatedRecipes = this.state.recipes
+        updatedRecipes.shift()
+        this.setState({ recipes: updatedRecipes })
+        this.checkRecipiesArray()
+    }
+
+    addRecipeToMatched = () => {
+        let matched = this.state.matchedRecipes
+        let recipes = this.state.recipes
+        matched.push(recipes.splice(0, 1)[0]);
+        this.setState({ matchedRecipes: matched })
+        this.checkRecipiesArray()
+    }
 
     render() {
         let { loading } = this.state;
@@ -29,15 +46,32 @@ class Game extends React.Component {
                 { loading ?
                     <Segment>
                         <Dimmer active>
-                            <Loader indeterminate>Preparing Files</Loader>
+                            <Loader active inline='centered' indeterminate>Preparing Files</Loader>
                         </Dimmer>
                     </Segment>
                     :
-                    <Container textAlign="center">
-                        <Card
-                            image={this.state.recipes[0].smallImageUrls[0]}
-                            header={this.state.recipes[0].recipeName} 
-                        />
+                    <Container>
+                        { this.state.recipes.length > 0 &&
+                        <Grid centered columns={2}>
+                            <Grid.Column>
+                                <Card
+                                    header={this.state.recipes[0].recipeName} 
+                                /> 
+                            </Grid.Column>
+                            <Grid.Row>
+                                <Grid.Column>
+                                    <Button size='massive' color='green' icon onClick={this.addRecipeToMatched}>
+                                        <Icon name='thumbs outline up' />
+                                    </Button>
+                                </Grid.Column>
+                                <Grid.Column>
+                                    <Button size='massive' color='red' icon onClick={this.removeRecipe}>
+                                        <Icon name='thumbs outline down' />
+                                    </Button>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                        }   
                     </Container>
                 }
             </div>
